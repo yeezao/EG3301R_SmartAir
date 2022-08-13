1f19da4dc47e5168d3f33f6d508b7d10538e2fc4
yeezao <liangyuzhao1999@gmail.com>
2022-08-13 18:32:41 +0800

    add subscribe, on_message and json methods

--- __main__.py
+++ __main__.py
@@ -1,46 +1,76 @@
+from re import L
 import paho.mqtt.client as mqtt
 import json
 import time
+from queue import Queue
 
+MQTT_TOPIC_SUB = "sensor_data"
+MQTT_TOPIC_PUB = "action"
+NUM_OF_SENSORS = 2
 
-def do_stuff():
-    time.sleep(1)
+sensor_dict = {}
 
-'''Callback to print error if connection fails'''
+# decode json string into objects and add to sensor_dictionary
+def json_serialize_add(client, message):
+    dic = json.loads(message)
+    sensor_dict[client] = dic
+    print(sensor_dict)
+
+def on_sensor_message(client, userdata, message):
+    print("Message received from Sensor", str(message.payload.decode("utf-8")))
+    json_serialize_add(client, message)
+
+# subscribe broker to sensors topic
+def subscribe_to_topic():
+    client1.subscribe(MQTT_TOPIC_SUB)
+    client2.subscribe(MQTT_TOPIC_SUB)
+    client1.on_message = on_sensor_message  
+    client2.on_message = on_sensor_message
+    #client1.
+
+# Callback to print error if connection fails
 def on_connect(client, ret):
     if ret != 0:
         print("Connection failed with error", ret)
+    else:
+        client.loop_start()
 
-'''Connect clients to broker'''
+# Connect clients to broker
 def connect_to_broker(client):
     client.on_connect = on_connect
-    client.connect("pi")
-    client.loop_start()
+    print("Connecting to client", client)
+    client.connect("pi", 1883)
+    #client.loop_start()
     while not client.is_connected:
         time.sleep(1)
-    client.loop_end()
+    #client.loop_end()
 
 
-'''Initialise clients and begin connection'''
+# Initialise clients and begin connection
 def setup():
-
     global client1, client2
     client1 = mqtt.Client("Sensor1")
     client2 = mqtt.Client("Sensor2")
     connect_to_broker(client1)
     connect_to_broker(client2)
+    subscribe_to_topic()
 
+def process_sensor_data():
+    time.sleep(1)
+    # TODO: process sensor data here
 
 def main():
     
     setup()
 
     while(1):
-        do_stuff()
+        if (len(dict) == NUM_OF_SENSORS):
+            print("All sensors have transmitted data - beginning processing")
+            process_sensor_data()
+
+    #client1.loop_forever()
+    #client2.loop_forever()
         
-
-
-
 
 if __name__ == "__main__":
     main()
