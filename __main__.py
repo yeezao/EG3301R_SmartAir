6a77ad911e9286c46c3c507dcaf7a1284505202a
Liang Yuzhao <liangyuzhao1999@gmail.com>
2022-08-22 23:22:00 +0800

    change callback logic

--- __main__.py
+++ __main__.py
@@ -43,8 +43,7 @@ def json_deserialise(object):
 # subscribe broker to sensors topic
 def subscribe_to_topic(client):
     client.subscribe(MQTT_TOPIC_SUB)
-    client.on_message = on_sensor_message
-    #client.on_publish = on_publish
+    print("subscribed and callback linked")
 
 # Connect clients to broker
 def connect_to_broker(client):
@@ -58,7 +57,7 @@ def connect_to_broker(client):
         print("Still attempting to connect to ", client)
     print("conn success")
     #client.loop_end()
-    client.loop_start()
+    #client.loop_start()
 
 # Initialise clients and begin connection
 def setup():
@@ -68,16 +67,14 @@ def setup():
 
     global client1
     client1 = mqtt.Client("processor")
-    #client2 = mqtt.Client("Sensor2")
+    client1.on_message = on_sensor_message
+    client1.on_publish = on_publish
     connect_to_broker(client1)
-    #connect_to_broker(client2)
-    subscribe_to_topic(client1)
 
 def process_sensor_data():
     sensor_data_processor = SensorDataProcessor()
     action_dict = sensor_data_processor.process_data(sensor_dict)
     #send_actions(action_dict)
-    #data_in_processing = False
 
 def send_actions(action_dict):
     if (action_dict[FILTER]):
@@ -98,42 +95,29 @@ def process_relay_action():
     elif action_dict[RELAY_1] == -1:
         GPIO.output(RELAY_2_PIN, GPIO.LOW)
 
-# Callback for when broker receives a message from client
-def on_sensor_message(client, userdata, message):
-    print("raw binary data received: ", message)
-    print("Message received from Sensor", str(message.payload.decode("utf-8")))
-    json_serialize_add(client, message)
-
-# Callback after action is sent
-def on_publish(client, userdata, result):
-    print("Action sent. Result of sending is ", result)
-
-
-# Callback to print error if connection fails
-def on_connect(client, userdata, flags, ret):
-    if ret != 0:
-        print("Connection failed with error ", client, " - ", ret)
-    else:
-        print("Connection Successful with message ", ret, "from client ", client)
-        #client.loop_forever()
-
 def main():
     
     setup()
+
+    global data_in_processing
+    data_in_processing = False
 
     while(1):
         print(len(sensor_dict))
         if (len(sensor_dict) == NUM_OF_SENSORS):
             print("All sensors have transmitted data - beginning processing") 
-            #data_in_processing = True
+            data_in_processing = True
             process_sensor_data()
             time.sleep(2)
+            sensor_dict.clear()
+            print("Sensor dict cleared")
+            data_in_processing = False
         else:
             print("Not all sensor data received")
             #GPIO.output(RELAY_1_PIN, GPIO.HIGH)
-            time.sleep(2)
+            time.sleep(4)
             #GPIO.output(RELAY_1_PIN, GPIO.LOW)
-            time.sleep(2)
+            time.sleep(4)
 
     #client1.loop_forever()
     #client2.loop_forever()
@@ -149,8 +133,13 @@ def on_connect(client, userdata, flags, ret):
     if ret != 0:
         print("Connection failed with error ", client, " - ", ret)
     else:
+        subscribe_to_topic(client1)
         print("Connection Successful with message ", ret, "from client ", client)
-        client.loop_start()
+        #client.loop_start()
+
+# Callback after action is sent
+def on_publish(client, userdata, result):
+    print("Action sent. Result of sending is ", result)
 
 if __name__ == "__main__":
     main()
