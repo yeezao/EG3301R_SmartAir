8e71dacba1ea8253221177bec259ee1d88055840
yeezao <liangyuzhao1999@gmail.com>
2022-10-03 23:00:07 +0800

    modify server code to accept PM data + refactor

--- SensorDataProcessor.py
+++ SensorDataProcessor.py
@@ -13,7 +13,7 @@ class SensorDataProcessor:
         action_dict = {pc.FILTER: 0, pc.RELAY_1: 0, pc.RELAY_2: 0}
         print(sensor_dict_multipleperiods)
 
-        self.rebase_co2_thresholds()
+        #self.rebase_co2_thresholds()
         avg_dict = {}
 
         for record_time, sensor_dict_singletime in sensor_dict_multipleperiods.items():
@@ -38,25 +38,30 @@ class SensorDataProcessor:
         voc_list = []
         temp_list = []
         humidity_list = []
+        pm1_list = []
+        pm25_list = []
+        pm10_list = []
         
         for item in dict.values():
             c02_list.append(item["CO2"])
             voc_list.append(item["TVOC"])
             temp_list.append(item["Temp"])
             humidity_list.append(item["Humidity"])
+            pm1_list.append(item["PM1"])
+            pm25_list.append(item["PM2.5"])
+            pm10_list.append(item["PM10"])
         
-        if pgv.prog_mode == pgv.ProgMode.AVG_CASE:
-            return_co2 = self.avg_list(c02_list)
-            return_voc = self.avg_list(voc_list)
-            return_temp = self.avg_list(temp_list)
-            return_humidity = self.avg_list(humidity_list)
-        elif pgv.prog_mode == pgv.ProgMode.WORST_CASE:
-            return_co2 = self.worst_case_list(c02_list)
-            return_voc = self.worst_case_list(voc_list)
-            return_temp = self.worst_case_list(temp_list)
-            return_humidity = self.worst_case_list(humidity_list)
+        return_dic = {}
 
-        return {"Temp": return_temp, "Humidity": return_humidity, "CO2": return_co2, "TVOC": return_voc}
+        return_dic["Temp"] = self.find_list_mode(c02_list)
+        return_dic["TVOC"] = self.find_list_mode(voc_list)
+        return_dic["Temp"] = self.find_list_mode(temp_list)
+        return_dic["Humidity"] = self.find_list_mode(humidity_list)
+        return_dic["PM1"] = self.find_list_mode(pm1_list)
+        return_dic["PM2.5"] = self.find_list_mode(pm25_list)
+        return_dic["PM10"] = self.find_list_mode(pm10_list)
+
+        return return_dic
 
     def determine_action(self, action_dict, avg_dict):
         if avg_dict["CO2"] >= pgv.co2_upper_bound or avg_dict["TVOC"] > 2000:
@@ -73,12 +78,18 @@ class SensorDataProcessor:
         elif avg_dict["TVOC"] < 800:
             action_dict[pc.FILTER] = -1
 
-    def rebasse_co2_thresholds(self):
+    def rebase_co2_thresholds(self):
         if (len(pgv.co2_values_rebaseline) == 0):
             return
         else:
             pgv.co2_upper_bound = self.avg_list(pgv.co2_values_rebaseline) + 700
             pgv.co2_lower_bound = pgv.co2_upper_bound * 0.8
+
+    def find_list_mode(self, aqlist):
+        if pgv.prog_mode == pgv.ProgMode.AVG_CASE:
+            return self.avg_list(aqlist)
+        if pgv.prog_mode == pgv.ProgMode.WORST_CASE:
+            return self.worst_case_list(aqlist)
 
     def avg_list(self, aqlist):
         return sum(aqlist) / len(aqlist)
